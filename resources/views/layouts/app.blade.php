7412a8fe3a9986937823234a7fd5f7be6cdea8af
Andrei <andreipaulc@gmail.com.com>
2026-06-16 13:02:38 +0300

    am adaugat ca pe desktop sa fie button toggle si pe mobile sa fie dupa sistemul de operare dark mode-ul

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data="{ dark:localStorage.getItem('dark') === 'true' }" x-init="$watch('dark', val => localStorage.setItem('dark',val))" :class="{ 'dark': dark }">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" 
+    x-data="{ dark: window.innerWidth >= 640 ? (localStorage.getItem('dark') === 'true') : (window.matchMedia('(prefers-color-scheme: dark)').matches}" 
+    x-init="if (window.innerWidth >= 640) {
+        $watch('dark', val => localStorage.setItem('dark', val))
+    }" 
+    :class="{ 'dark': dark }">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
